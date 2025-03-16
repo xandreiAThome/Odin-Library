808e158f1a7cc9a1085f86da6a9e797cdf2e741a
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-03-16 21:11:11 +0800

    feat: created form

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const myLibrary = [];
 const mainDiv = document.querySelector("main");
 const addModal = document.getElementById("addModal");
+const addBookButton = document.getElementById("submit");
+const addButton = document.getElementById("add");
+const bookForm = document.getElementById("bookForm");
 
 function Book(name, author, pages, read) {
   if (!new.target) {
@@ -9,21 +12,37 @@ function Book(name, author, pages, read) {
 
   this.name = name;
   this.author = author;
-  this.id = crypto.randomUUID;
+  this.id = crypto.randomUUID();
   this.pages = pages;
   this.read = read;
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-}
-
 function displayBooks() {}
 
-const addButton = document.getElementById("add");
 addButton.addEventListener("click", () => {
-  console.log("hi");
   addModal.showModal();
+});
+
+bookForm.addEventListener("submit", (e) => {
+  const formData = new FormData(e.target);
+  const entries = Object.fromEntries(formData);
+
+  const finishedReading = entries.read === "on" || false;
+
+  const newBook = new Book(
+    entries.title,
+    entries.author,
+    entries.pages,
+    finishedReading
+  );
+
+  myLibrary.push(newBook);
+
+  console.log(myLibrary);
+
+  addModal.close();
+
+  bookForm.reset();
 });
 
 window.onclick = function (event) {
